b0ee5c90fbe65a8812dfd20e9390ccef84102a87
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-02-22 18:25:30 +0100

    animation vers nouvelle page ok
    suppression de classes inutiles

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -2,23 +2,21 @@ $(document).ready(function () {
     
     $("#hexalist").on("click", function (e) {
     e.preventDefault(); //on empêche le chargement du lien 
-    
-let chosenOne=e.target.parentNode.getAttribute('id') || e.target.id || e.target.parentNode.parentNode.getAttribute('id') //on repère le 'li'cliqué
-console.log(chosenOne);
-/*preparation de la recup de l url*/
-
-let url= e.target.parentNode.getAttribute("href") || e.target.href || e.target.parentNode.parentNode.getAttribute('href');
-console.log(url);
+  
+        let mylink= e.target.querySelector("a"); 
+        let myurl=mylink.getAttribute("href");
+        let myHexa=e.target.parentNode.getAttribute('id') || e.target.id || e.target.parentNode.parentNode.getAttribute('id') //on repère le 'li'cliqué
 
 $("li").toggleClass('fade');
-$('#'+chosenOne).removeClass('fade');
+$('#'+myHexa).removeClass('fade');
 $('.fade').fadeOut('slow');
-$('#'+chosenOne).addClass('visible');
-$('#'+chosenOne).css({'transform': 'scale(30)', 'transition':'all 500ms ease-out'}); // à améliorer...
-$('.link').fadeOut(1000,'linear');
+$('#'+myHexa).addClass('visible');
+$('#'+myHexa).css({'transform': 'scale(30)', 'transition':'all 500ms ease-out'});
+$('.link').fadeOut(1000, function(){
+    window.location.href=myurl;
+});
+  
 
-$(':root').load(url, function(result) {
-   $(':root').html(result);
+});
 });
 
-    })})
