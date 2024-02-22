608627d885169c442ea7f5d8cc0a78d704015a35
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-02-22 16:09:42 +0100

    hop hop hop

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -4,16 +4,21 @@ $(document).ready(function () {
     e.preventDefault(); //on empêche le chargement du lien 
     
 let chosenOne=e.target.parentNode.getAttribute('id') || e.target.id || e.target.parentNode.parentNode.getAttribute('id') //on repère le 'li'cliqué
-
+console.log(chosenOne);
 /*preparation de la recup de l url*/
 
 let url= e.target.parentNode.getAttribute("href") || e.target.href || e.target.parentNode.parentNode.getAttribute('href');
-
+console.log(url);
 
 $("li").toggleClass('fade');
 $('#'+chosenOne).removeClass('fade');
 $('.fade').fadeOut('slow');
 $('#'+chosenOne).addClass('visible');
-$('#'+chosenOne).css({'transform': 'scale(15)', 'transition':'all 5s ease-in-out'}); // à améliorer...
-$('span.lien').fadeOut(1000,'linear');
+$('#'+chosenOne).css({'transform': 'scale(30)', 'transition':'all 500ms ease-out'}); // à améliorer...
+$('.link').fadeOut(1000,'linear');
+
+$(':root').load(url, function(result) {
+   $(':root').html(result);
+});
+
     })})
