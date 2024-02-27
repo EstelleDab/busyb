0c6126bb2da7c5b8b22944d076f922a7d89025b4
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-02-27 13:17:03 +0100

    centrage

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -9,10 +9,11 @@ $(document).ready(function () {
         let myHexa=e.target.parentNode.getAttribute('id') || e.target.id || e.target.parentNode.parentNode.getAttribute('id') //on repère le 'li'cliqué
 
 $("li").toggleClass('fade');
-//$('#'+myHexa).removeClass('fade');
+$('#'+myHexa).removeClass('fade');
 $('.fade').fadeOut('slow');
 $('#'+myHexa).addClass('visible');
 $('#'+myHexa).css({'transform': 'scale(30)', 'transition':'all 500ms ease-out'});
+$('a').fadeOut(500);
 $('.link').fadeOut(1000,function(){
 
     window.location.href=myurl;
