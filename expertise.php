77a8943452c65850b4be5ffb45795f64ff77ba36
Amanda Héry-Prieto <amandaheryprieto@gmail.com>
2024-03-05 17:51:37 +0200

    Ajout page expertise.php

--- expertise.php
+++ expertise.php
@@ -1,39 +1,67 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Page expertise</title>
     <?php include 'inc.head.php';?>
 </head>
+
 <body>
-    <h1>Notre expertise</h1>
-    
-    <div class="parallax parallax1"></div>
 
-    <div>
-        <h2>Création d'Identité de Marque</h2>
-        <p>Votre identité visuelle est la première impression que vous laissez à vos clients. Chez Busy B, nous nous spécialisons dans la création d'identités de marque mémorables et distinctives. Du design de logo à la palette de couleurs, nous travaillons en collaboration avec vous pour donner vie à une identité qui reflète l'essence même de votre entreprise. </p>
-    </div>
-    
-    <div class="parallax parallax2"></div>
 
-    <div>
-        <h2>Création d'Identité de Marque</h2>
-        <p>Votre identité visuelle est la première impression que vous laissez à vos clients. Chez Busy B, nous nous spécialisons dans la création d'identités de marque mémorables et distinctives. Du design de logo à la palette de couleurs, nous travaillons en collaboration avec vous pour donner vie à une identité qui reflète l'essence même de votre entreprise. </p>
-    </div>
+    <div class="wrapper">
+        <header class="header-expertise">
+            <img src="assets/images/expertise/sky.jpg" class="background-far">
+            <img src="assets/images/expertise/field.png" class="background">
+            <img src="assets/images/expertise/bee.png" class="foreground">
+            <h1>Notre expertise</h1>
+        </header>
 
-    <div class="parallax parallax3"></div>
+        <div class="wbg">
+            <section class="section-expertise">
 
-    <div>
-        <h2>Création d'Identité de Marque</h2>
-        <p>Votre identité visuelle est la première impression que vous laissez à vos clients. Chez Busy B, nous nous spécialisons dans la création d'identités de marque mémorables et distinctives. Du design de logo à la palette de couleurs, nous travaillons en collaboration avec vous pour donner vie à une identité qui reflète l'essence même de votre entreprise. </p>
-    </div>
+                <p>Bienvenue chez Busy B, votre partenaire privilégié pour donner vie à votre présence en ligne de manière exceptionnelle. Chez Busy B, nous croyons en la puissance de la créativité, de l'innovation et de la technologie pour propulser votre entreprise vers de nouveaux horizons sur le web. Que vous cherchiez à établir une identité de marque distinctive, à développer un site web sur mesure, à optimiser votre visibilité grâce au marketing numérique, ou à améliorer l'expérience utilisateur, notre équipe dévouée est là pour transformer vos idées en réalité. Nous allions expertise technique et sens artistique pour créer des solutions web uniques, adaptées à vos besoins spécifiques. Explorez nos services diversifiés et découvrez comment Busy B peut être le catalyseur de votre succès numérique. Prêts à relever tous les défis, nous sommes impatients de collaborer avec vous pour façonner une présence en ligne qui vous démarque et qui séduit votre audience. Bienvenue dans l'univers Busy B, où l'innovation rencontre l'excellence web.</p>
 
-    <div class="parallax parallax4"></div>
+                    <div class="content-wrapper">
+                        <div class="content">
+                            <div class="text">
+                                <h2>Création d'Identité de Marque</h2>
+                                <p>Notre équipe de designers talentueux travaille en collaboration avec vous pour donner vie à une identité visuelle qui reflète l'essence même de votre entreprise. Du design de logo à la palette de couleurs, nous sommes là pour créer une image distinctive qui fera une impression durable sur vos clients.</p>
+                            </div>
+                            <div class="parallax parallax1"></div>
+                        </div>
+                    
+                        <div class="content">
+                            <div class="parallax parallax2"></div>
+                            <div class="text">
+                                <h2>Développement Web sur Mesure</h2>
+                                <p>Busy B excelle dans le développement web sur mesure. Que vous ayez besoin d'un site vitrine élégant, d'une boutique en ligne robuste ou d'une plateforme complexe, notre équipe technique est prête à transformer vos idées en réalité. Des solutions personnalisées pour répondre à vos besoins spécifiques.</p>
+                            </div>
+                        </div>
+                    
+                        <div class="content">
+                            <div class="text">
+                                <h2>Stratégie Marketing Numérique</h2>
+                                <p>Maximisez votre visibilité en ligne avec notre expertise en stratégie marketing numérique. De la gestion des médias sociaux à l'optimisation des moteurs de recherche (SEO), Busy B vous guide pour atteindre vos objectifs marketing et accroître votre présence sur le web.</p>
+                            </div>
+                            <div class="parallax parallax3"></div>
+                        </div>
 
-    <div>
-        <h2>Création d'Identité de Marque</h2>
-        <p>Votre identité visuelle est la première impression que vous laissez à vos clients. Chez Busy B, nous nous spécialisons dans la création d'identités de marque mémorables et distinctives. Du design de logo à la palette de couleurs, nous travaillons en collaboration avec vous pour donner vie à une identité qui reflète l'essence même de votre entreprise. </p>
-    </div>
+                        <div class="content">
+                            <div class="parallax parallax4"></div>
+                            <div class="text">
+                                <h2>UI & UX Design</h2>
+                                <p>Nous accordons une importance particulière à l'expérience utilisateur et au design d'interface. En combinant esthétique et fonctionnalité, nous créons des interfaces conviviales qui captivent les utilisateurs et renforcent la satisfaction client.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+
+            </section>
+
+        </div>
 
 </body>
+
 </html>
--- inc.head.php
+++ inc.head.php
@@ -4,4 +4,5 @@
 	<link rel="icon" type="image/png" href="assets/logo.png"/>
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
    <link href="assets/css/style.css" rel="stylesheet">
+   <link href="assets/css/expertise.css" rel="stylesheet">
 </head>
