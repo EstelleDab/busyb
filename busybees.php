7d036c4e1f6e7895569ff2e056a688de8596c08d
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-05 10:06:37 +0100

    premiere mise en forme hexa team

--- busybees.php
+++ busybees.php
@@ -6,35 +6,34 @@
 </head>
 <body class="bg-dark vw-auto h-100">
     <?php include 'inc.header.php';?>
-
-    <h1 class='bg-dark text-primary' >L'ÉQUIPE DE BUSY BEES</h1>
-    <section class='h-100 bg-white'>
-    <h2 class='text-secondary'>Présentation de l'équipe</h2>
-    <div class='d-flex flex-row justify-content-center align-items-center'>
-        <div class='hex-gap d-flex flex-column  align-items-center'>
-            <div class=' hex blue '>firstboug</div>
-            <div class=' hex pink'>firstboug</div>
-            <div class='hex yellow'>third</div>   
-        </div>     
-        <div class='hex-gap d-flex flex-column  align-items-center'>   
-            <div class=' hex pink '>second</div>
-            <div class=' hex blue'>firstboug</div>
-     
-        </div>    
-        <div class='hex-gap d-flex flex-column  align-items-center'>  
-            <div class='hex yellow'>third</div>
-            <div class=' hex blue'>firstboug</div>
-            <div class='hex pink'>third</div>
-        </div>     
-        
-        <div class='hex-gap d-flex flex-column  align-items-center'>   
-            <div class='hex pink '>second</div>
-        </div>    
-        
-    </div> 
-
-
+    
+      
+    <section class='h-100 bg-white '>
+    <h1 class='bg-dark text-primary px-5' >L'ÉQUIPE DE BUSY BEES</h1>
+        <h2 class='text-secondary px-5'>Présentation de l'équipe</h2>
+        <div class='h-50 px-5'>
+            <p>
+blabla
+            </p>
+        </div>
+        <div class='d-flex flex-row justify-content-center align-items-center  team-gap '>
+            <div class='d-flex flex-column  align-items-center'>
+                <div id='ama'class='hex-team'>first</div>
+                <div class='hex-team invisible'></div>   
+            </div>     
+             <div id='fra' class='d-flex flex-column  align-items-center'>   
+                <div class=' hex-team'>fourth</div>
+            </div>    
+            <div id='ste' class='d-flex flex-column  align-items-center'>  
+                <div class='hex-team'>sixth</div>
+                <div class='hex-team invisible'></div>
+            </div>     
+            <div class='d-flex flex-column  align-items-center'>   
+                <div class='hex-team'>nineth</div>
+            </div>    
+        </div> 
     </section>
+
     <?php include 'inc.footer.php';?>
 
 </body>
--- index.php
+++ index.php
@@ -11,18 +11,34 @@
 
 				<div class="hex-gap h-100 d-flex flex-row justify-content-center align-items-center">
 
-					<div class="hex-gap d-flex flex-column  align-items-center">
+					<div class=" d-flex flex-column  align-items-center">
 						<li class="hex pink">&nbsp;</li>
 						<li id="js-hexa1" class="hex hex-nav yellow"><span class="link"><a class='nav-link' >contact</a></span></li>
 					</div>
+<<<<<<< Updated upstream
 					<div class="hex-gap d-flex flex-column  align-items-center">
 						<li id="js-hexa2" class="hex hex-nav yellow"><span class="link"><a class='nav-link' href="expertise.php">notre expertise</a></span></li>
 						<li id="js-hexa3" class="hex blue main-hex"><img src='assets/images/bee-logo.png'></li>
 		  				<li id="js-hexa4" class="hex hex-nav black"><span class="link"><a class='nav-link'>travaillons ensemble</a></span></li>
 					</div>
 					<div class="hex-gap d-flex flex-column align-items-center">
+<<<<<<< Updated upstream
 						<li id="js-hexa5"  class="hex hex-nav black "><span class="link"><a class='nav-link' href="busybees.php">busy bees</a></span></li>
 						<li id="js-hexa6" class="hex hex-nav  blue "><span class="link"><a class='nav-link'>portfolio</a></span></li>
+=======
+						<li id="hexa5"  class="hex hex-nav black "><span class="link"><a class='nav-link' href="equipe.php">busy bees</a></span></li>
+						<li id="hexa6" class="hex hex-nav  blue "><span class="link"><a class='nav-link'>portfolio</a></span></li>
+=======
+					<div class=" d-flex flex-column  align-items-center">
+						<li id="js-hexa2" class="hex hex-nav yellow"><span class="link"><a class='nav-link' href="expertise.php">notre expertise</a></span></li>
+						<li id="js-hexa3" class="hex blue main-hex"><img src='assets/images/bee-logo.png'></li>
+		  				<li id="js-hexa4" class="hex hex-nav black"><span class="link"><a class='nav-link'>travaillons ensemble</a></span></li>
+					</div>
+					<div class="d-flex flex-column align-items-center">
+						<li id="js-hexa5"  class="hex hex-nav black "><span class="link"><a class='nav-link' href="busybees.php">busy bees</a></span></li>
+						<li id="js-hexa6" class="hex hex-nav  blue "><span class="link"><a class='nav-link'>portfolio</a></span></li>
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 		 	 		</div> 
 	  		</div>
 			</ul>
