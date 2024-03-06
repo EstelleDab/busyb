0ea7d3355300a38feb13f45babad26f376586fb0
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-06 11:38:49 +0100

    lien vers css bootstrap dans header

--- inc.head.php
+++ inc.head.php
@@ -4,5 +4,10 @@
 	<link rel="icon" type="image/png" href="assets/logo.png"/>
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
    <link href="assets/css/style.css" rel="stylesheet">
+<<<<<<< Updated upstream
    <link href="assets/css/expertise.css" rel="stylesheet">
+=======
+   <link href="assets/css/busybees.css" rel="stylesheet">
+   <link href="assets/css/bootstrap.css" rel="stylesheet">
+>>>>>>> Stashed changes
 </head>
