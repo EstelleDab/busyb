9a94463c0b55ffe8ded7842cf3e6e26ddb2dba32
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-07 08:28:25 +0100

    plop

--- busybees.php
+++ busybees.php
@@ -4,13 +4,13 @@
     <title>Page équipe</title>
     <?php include 'inc.head.php';?>
 </head>
-<body class="bg-dark vw-auto">
+<body class="bg-dark">
     <?php include 'inc.header.php';?>
     
-    <section class='bg-light h-100'>
+    <main class='bg-light'>
         <h1 class='bg-dark text-primary px-5' >L'ÉQUIPE DE BUSY</h1>
         <h2 class='text-secondary px-5'>Présentation de l'équipe</h2>
-        <div class='h-100 d-flex flex-column justify-content-start'>
+        <div class='d-flex flex-column justify-content-start'>
             <div class='px-5'>
                 <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     Nisi lacus sed viverra tellus in hac. Aliquet enim tortor at auctor urna. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. 
@@ -19,9 +19,9 @@
                     Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. At risus viverra adipiscing at. Mattis aliquam faucibus purus in massa tempor nec. Ut morbi tincidunt augue interdum velit euismod. Sit amet dictum sit amet justo donec. Dictum non consectetur a erat nam at.
                     Orci sagittis eu volutpat odio facilisis mauris. Mattis vulputate enim nulla aliquet porttitor lacus. Porta lorem mollis aliquam ut porttitor leo a diam. Interdum consectetur libero id faucibus nisl tincidunt. Accumsan tortor posuere ac ut. Metus aliquam eleifend mi in nulla. Risus quis varius quam quisque id. Sed sed risus pretium quam vulputate dignissim suspendisse in est. Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Aliquet risus feugiat in ante metus. Mattis rhoncus urna neque viverra justo nec ultrices dui. Auctor elit sed vulputate mi sit amet mauris commodo. Sodales ut eu sem integer vitae justo. Tortor condimentum lacinia quis vel eros donec ac odio tempor. </p>
             </div>
-            <div class='hexaline  px-5 mx-5 mb-4 mt-4 d-flex flex-row justify-content-start align-items-center '>
+            <div class='h-50 hexaline px-5 mx-5 mb-4 mt-4 d-flex flex-row justify-content-start align-items-center '>
                 <div class='carte'>
-                    <div id=js-ama class='carte__inner mb-5 d-flex flex-column align-items-center'>
+                    <div  id='js-ama' class='carte__inner mb-5'>
                                 <div class="carte__face carte__face--front d-flex align-items-center justify-content-center">
                                     <img src="assets/images/amanda.jpg" alt="photo-amandine">
                                 </div>
@@ -41,34 +41,15 @@
                                     </div>
                                 </div>
                     </div>
-                    <div id=js-fra class='carte__inner mb-5 d-flex flex-column align-items-center'>
-                                <div class="carte__face carte__face--front d-flex align-items-center justify-content-center">
-                                    <img src="assets/images/fra.png" alt="photo-francis">
-                                </div>
-                                <div class="carte__face carte__face--back d-flex flex-column align-items-center justify-content-center bg-primary">
-                                    <div class='carte__content d-flex flex-column align-items-center justify-content-center '>
-                                        <div class='carte__header'>
-                                            <h3>Francis<h3>
-                                        </div>
-                                        <div class='carte__body'>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                                                    labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac. 
-                                                    Nec dui nunc mattis enim. Sit amet consectetur adipiscing elit ut aliquam purus. 
-                                                    Felis eget nunc lobortis mattis. 
-                                                </p>
-                                                
-                                        </div>
-                                    </div>
-                                </div>
-                    </div>
-                    <div id=js-ste class='carte__inner'>
+                <div class='carte'>
+                    <div id='js-ste' class='carte__inner mb-5'>
                                 <div class="carte__face carte__face--front d-flex align-items-center justify-content-center">
                                     <img src="assets/images/estelle.jpeg" alt="photo-stella">
                                 </div>
                                 <div class="carte__face carte__face--back d-flex flex-column align-items-center justify-content-center bg-primary">
                                     <div class='carte__content d-flex flex-column align-items-center justify-content-center '>
                                         <div class='carte__header'>
-                                            <h3>Stella<h3>
+                                            <h3>Estelle<h3>
                                         </div>
                                         <div class='carte__body'>
                                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
@@ -81,6 +62,28 @@
                                     </div>
                                 </div>
                     </div>
+                    <div class='carte'>
+                    <div id='js-fra' class='carte__inner mb-5'>
+                                <div class="carte__face carte__face--front d-flex align-items-center justify-content-center">
+                                    <img src="assets/images/fra.png" alt="photo-francis">
+                                </div>
+                                <div class="carte__face carte__face--back d-flex flex-column align-items-center justify-content-center bg-primary">
+                                    <div class='carte__content d-flex flex-column align-items-center justify-content-center '>
+                                        <div class='carte__header'>
+                                            <h3>Françis<h3>
+                                        </div>
+                                        <div class='carte__body'>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                                                    labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac. 
+                                                    Nec dui nunc mattis enim. Sit amet consectetur adipiscing elit ut aliquam purus. 
+                                                    Felis eget nunc lobortis mattis. 
+                                                </p>
+                                                
+                                        </div>
+                                    </div>
+                                </div>
+                    </div>
+                   
                 </div>
 </div>
             <!--
@@ -105,9 +108,11 @@
                 </div>   
             </div> -->
         </div>
-    </section>
+</main>
+    <?php include 'inc.footer.php';?> 
+
     <script src="assets/js/anim.js"></script>	
    <script src="assets/libraries/jquery-3.7.1.min.js"></script>
-    <?php include 'inc.footer.php';?>
+
 </body>
 </html>
--- inc.footer.php
+++ inc.footer.php
@@ -1,19 +1,17 @@
-<footer class="h-25 text-tertiary bottom bg-transparent ">
-    <div class=" h-50 mb-0 bg-transparent d-flex flex-row justify-content-center align-items-center">
+<footer class='h-25 text-tertiary position-static bottom-0 bg-transparent'>
+    <div class="  mb-0 bg-transparent d-flex flex-row justify-content-center align-items-center">
       	<div class= " d-flex flex-row ">
-          <div class= "px-4">
-            <p class="footer-text text-info" >Politique de confidentialité</p>
-          </div>
+          	<div class= "px-4">
+            	<p class="footer-text text-info" >Politique de confidentialité</p>
+          	</div>
 
-          <div class= "px-4">
-            
-  <p class="footer-text text-info px-5">Mentions Légales</p>
-
-		  </div>
-        </div>
+          	<div class= "px-4">
+        		<p class="footer-text text-info px-5">Mentions Légales</p>
+			</div>
+    	</div>
 
         <!-- Footer RS -->
-        <div class="gap d-flex flex-column">
+        <div class="d-flex flex-column align-items-center">
           
 			<div class="px-5 d-flex flex-row" >
 				<div class= "px-3">
@@ -21,9 +19,7 @@
               		<img  src="assets/images/icons/facebook.png" alt="logo-facebook">
             	</a>
           		</div>
-
 				<div class= "px-3">
-
             		<a class="footer-text"  href="#" title="instagram">
               		<img src="assets/images/icons/instagram.png" alt="logo-instagram">
             		</a>
