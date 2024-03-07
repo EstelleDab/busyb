cdf7dd93544a2c8ce7bc205c8c2e608f2fc330b3
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-07 11:47:05 +0100

    modif pages + busybees

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Busy B Agence Digitale</title>
+		<title>Busy B Agence Digitale</title>  <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
 		<?php include 'inc.head.php';?>
 	</head>
 <body>
@@ -20,14 +21,26 @@
 						<li id="hexa3" class="hex blue main-hex"><img src='assets/images/bee-logo.png'></li>
 		  				<li id="hexa4" class="hex hex-nav black"><span class="link"><a class='nav-link'>travaillons ensemble</a></span></li>
 					</div>
+<<<<<<< Updated upstream
 					<div class="hex-gap d-flex flex-column align-items-center">
 						<li id="hexa5"  class="hex hex-nav black "><span class="link"><a class='nav-link' href="equipe.php">busy bees</a></span></li>
 						<li id="hexa6" class="hex hex-nav  blue "><span class="link"><a class='nav-link'>portfolio</a></span></li>
+=======
+					<div class="d-flex flex-column align-items-center">
+
+						<li id="js-hexa5"  class="hex hex-nav black "><span class="link"><a class='nav-link' href="pages/busybees.php">busy bees</a></span></li>
+						<li id="js-hexa6" class="hex hex-nav  blue "><span class="link"><a class='nav-link'>portfolio</a></span></li>
+>>>>>>> Stashed changes
 		 	 		</div> 
 	  		</div>
 			</ul>
 
 	</nav>
+<<<<<<< Updated upstream
+=======
+	<script src="assets/libraries/jquery-3.7.1.min.js"></script>
+	<script src="assets/js/anim.js"></script>	
+>>>>>>> Stashed changes
 
 	
 
