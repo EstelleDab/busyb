a3a5c930712607aa83d6a85fe0351a3662a2de50
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-07 17:38:49 +0100

    réglage du footer

--- inc.footer.php
+++ inc.footer.php
@@ -1,19 +1,20 @@
-<footer class='h-25 text-tertiary position-static bottom-0 bg-transparent'>
-    <div class="  mb-0 bg-transparent d-flex flex-row justify-content-center align-items-center">
-      	<div class= " d-flex flex-row ">
-          	<div class= "px-4">
+<footer class='h-25 d-flex flex-row justify-content-center text-tertiary position-static bottom-0 bg-transparent'>
+    
+	<div class="  mb-0 bg-transparent d-flex flex-row justify-content-center align-items-center">
+      	<div class= " d-flex flex-row">
+          	<div class= "px-3">
             	<p class="footer-text text-info" >Politique de confidentialité</p>
           	</div>
 
-          	<div class= "px-4">
+          	<div class= "px-3">
         		<p class="footer-text text-info px-5">Mentions Légales</p>
 			</div>
     	</div>
 
         <!-- Footer RS -->
-        <div class="d-flex flex-column align-items-center">
+        <div class="d-flex flex-column align-items-center justify-content-center">
           
-			<div class="px-5 d-flex flex-row" >
+			<div class="px-3 d-flex flex-row" >
 				<div class= "px-3">
             		<a class="footer-text"  href="#" title="facebook">
               		<img  src="../assets/images/icons/facebook.png" alt="logo-facebook">
