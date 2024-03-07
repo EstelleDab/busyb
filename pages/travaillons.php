cd25f11b2e8a450bb1376d35ac59513f23c8db77
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-07 15:41:44 +0100

    essais carousel bootstrap

---
+++ pages/travaillons.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Travaillons Ensemble</title>
+    <link href="../assets/css/travaillons.css" rel="stylesheet">
+    <?php include '../inc.head.php';?>
+</head>
+
+<body class="bg-dark">
+    <?php include '../inc.header.php';?>
+    <main>
+<div class='bg-light'>
+    <div id="myCarousel" class="carousel slide m-auto w-50" data-bs-ride="carousel">
+        <div class="carousel-inner d-flex flex-row bg-dark m-auto w-50 h-100">
+            
+            <div class="carousel-item d-flex flex-column align-items-center justify-content-center active "> 
+                    <p class='d-block text-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. BOUDY  viverra tellus in hac. 
+                    Nec dui nunc mattis enim. Sit amet consectetur adipiscing elit ut aliquam purus. 
+                    Felis eget nunc lobortis mattis. 
+                    </p>
+            </div>
+            
+
+            <div class="carousel-item d-flex flex-row align-items-center justify-content-center">
+                    <p class=' d-block text-light '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac. 
+                    Nec dui nunc mattis enim. Sit amet consectetur adipiscing elit ut aliquam purus. 
+                    Felis eget nunc lobortis mattis. 
+                    </p>
+           </div>
+            
+            
+            <div class="carousel-item d-flex flex-row align-items-center">
+                    <img src="../assets/images/stars.png" class="d-block" alt="photo-stars">
+                    <p class='d-block text-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua. Nisi lacus sed viverra tellus in hac. 
+                    Nec dui nunc mattis enim. Sit amet consectetur adipiscing elit ut aliquam purus. 
+                    Felis eget nunc lobortis mattis. 
+                    </p>
+            
+            </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon btn btn-secondary" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next " type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon btn btn-secondary" aria-hidden="true"></span>
+             <span class="visually-hidden">Next</span>
+        </button>
+</div>
+    </div>
+</div>
+
+</main>
+    <?php include '../inc.footer.php';?> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="../assets/libraries/jquery-3.7.1.min.js"></script>
+    <script src="../assets/js/anim.js"></script>	
+</body>
