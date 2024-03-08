0183dea9ec9d6461107972d067c1ca813f25d670
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2024-03-08 11:49:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 		<?php include 'inc.head.php';?>
 	</head>
 <body>
-<!--<h1 class='visually-hidden'>Agence Digitale Busy B</h1>
-<h2 class='visually-hidden'>Une rûche d'idées!</h2>-->
-	<nav class="h-100">
+<h1 class='visually-hidden'>Agence Digitale Busy B</h1>
+<h2 class='visually-hidden'>Une rûche d'idées!</h2>
+<div class='container'>
+	<nav class="">
 			<ul id="hexalist" class="h-100"  >
 
 				<div class="hex-gap h-100 d-flex flex-row justify-content-center align-items-center">
@@ -16,14 +17,12 @@
 						<li class="hex pink">&nbsp;</li>
 						<li id="js-hexa1" class="hex hex-nav yellow"><span class="link"><a class='nav-link' >contact</a></span></li>
 					</div>
-
 					<div class=" d-flex flex-column  align-items-center">
-						<li id="js-hexa2" class="hex hex-nav yellow"><span class="link"><a class='nav-link' href="expertise.php">notre expertise</a></span></li>
+						<li id="js-hexa2" class="hex hex-nav yellow"><span class="link"><a class='nav-link' href="expertise.php">notre texpertise</a></span></li>
 						<li id="js-hexa3" class="hex blue main-hex"><img src='assets/images/bee-logo.png'></li>
 		  				<li id="js-hexa4" class="hex hex-nav black"><span class="link"><a class='nav-link' href='pages/travaillons.php'>travaillons ensemble</a></span></li>
 					</div>
 					<div class="d-flex flex-column align-items-center">
-
 						<li id="js-hexa5"  class="hex hex-nav black "><span class="link"><a class='nav-link' href="pages/busybees.php">busy bees</a></span></li>
 						<li id="js-hexa6" class="hex hex-nav  blue "><span class="link"><a class='nav-link'>portfolio</a></span></li>
 		 	 		</div> 
