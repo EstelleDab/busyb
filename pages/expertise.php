b0e9a2d1e41fda25348ff9daaa745823aae49c07
Amanda Héry-Prieto <amandaheryprieto@gmail.com>
2024-03-15 14:58:33 +0200

    Page expertise design ruche

--- pages/expertise.php
+++ pages/expertise.php
@@ -12,16 +12,17 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
+
+
 <body class="bg-primary">
 
-
-<div class="bg-light">
-<h1 class="bg-primary text-dark px-5">Notre expertise</h1>
-<h2 class='text-secondary px-5'>Faites le buzz avec Busy B</h2>
-<div class='d-flex flex-column justify-content-start'>
+    <div class="bg-light">
+        <h1 class="bg-primary text-dark px-5">Notre expertise</h1>
+        <h2 class='text-secondary px-5'>Faites le buzz avec Busy B</h2>
+        <div class='d-flex flex-column justify-content-start'>
             <div class='px-5'>
 
-<p>Busy B, c'est votre partenaire privilégié dans le monde numérique en constante
+                <p>Busy B, c'est votre partenaire privilégié dans le monde numérique en constante
                     évolution. En tant
                     qu'agence web avant-gardiste, Busy B s'engage à offrir des solutions innovantes et personnalisées
                     pour répondre à vos besoins numériques les plus exigeants. Que vous aspiriez à une présence en ligne
@@ -30,111 +31,121 @@
                     notre gamme complète de services et découvrez comment Busy B peut transformer vos idées en réalité
                     numérique, propulsant votre entreprise vers de nouveaux sommets dans le paysage digital.</p>
 
-            </div>
-        </div>
+            </div><!-- eof px-5 -->
+        </div><!-- eof d-flex -->
 
-    <div class="container rounded p-5">
+        <div class="container-fluid p-5">
+            <div class="main-expertise parallax pb-5 rounded">
+                <div class="container-expertise p-5">
 
-
-        <div class="parallax p-5 rounded">
-
-
-
-            <div class="card-deck" data-aos="fade-right" data-aos-delay="1500" data-aos-duration="1000"><!-- 1ère ligne -->
-                <div class="card text-center border-0">
-                    <div class="card-body">
+                    <div class="text-center text-wrap p-4">
                         <i class="fa-solid fa-desktop fa-3x mb-3"></i><!--icone-->
                         <h3>Conception de Sites Web</h3>
-                        <p>Nous créons des sites web modernes et intuitifs pour représenter votre entreprise de manière
+                        <p>Nous créons des sites web modernes et intuitifs pour représenter votre entreprise de
+                            manière
                             professionnelle.</p>
-                    </div><!-- eof card-body -->
-                </div><!-- eof card -->
+                    </div>
 
-                <div class="card text-center border-0">
-                    <div class="card-body">
+                    <div class="text-center text-wrap p-4">
                         <i class="fa-solid fa-paint-brush fa-3x mb-3"></i><!--icone-->
                         <h3>Conception Graphique</h3>
-                        <p>Notre équipe de graphistes talentueux donne vie à votre marque avec des visuels accrocheurs
+                        <p>Notre équipe de graphistes talentueux donne vie à votre marque avec des visuels
+                            accrocheurs
                             et un design unique.</p>
-                    </div><!-- eof card-body -->
-                </div><!-- eof card -->
+                    </div>
 
-                <div class="card text-center border-0">
-                    <div class="card-body">
+                    <div class="text-center text-wrap p-4">
                         <i class="fa-solid fa-cogs fa-3x mb-3"></i><!--icone-->
                         <h3>Développement Web</h3>
-                        <p>Des solutions web personnalisées et évolutives pour répondre aux besoins spécifiques de votre
+                        <p>Des solutions web personnalisées et évolutives pour répondre aux besoins spécifiques de
+                            votre
                             entreprise.</p>
-                    </div><!-- eof card-body -->
-                </div><!-- eof card -->
-            </div><!-- eof card-deck -->
+                    </div>
 
-            <div class="card-deck mt-4" data-aos="fade-right" data-aos-delay="1000" data-aos-duration="1000"><!-- 2ème ligne -->
-                <div class="card text-center border-0">
-                    <div class="card-body">
+                    <div class="text-center text-wrap p-4">
                         <i class="fa-solid fa-search fa-3x mb-3"></i><!--icone-->
                         <h3>Référencement SEO</h3>
-                        <p>Optimisation pour les moteurs de recherche afin d'augmenter la visibilité de votre site et
+                        <p>Optimisation pour les moteurs de recherche afin d'augmenter la visibilité de votre site
+                            et
                             attirer un trafic qualifié.</p>
-                    </div><!-- eof card-body -->
-                </div><!-- eof card -->
 
-                <div class="card text-center border-0">
-                    <div class="card-body">
+                    </div>
+
+                    <div class="text-center text-wrap p-4">
                         <i class="fa-solid fa-mobile fa-3x mb-3"></i><!--icone-->
                         <h3>Applications Mobiles</h3>
                         <p>Développement d'applications mobiles intuitives pour rester connecté avec vos clients où
                             qu'ils soient.</p>
-                    </div><!-- eof card-body -->
-                </div><!-- eof card -->
 
-                <div class="card text-center border-0">
-                    <div class="card-body">
+                    </div>
+
+                    <div class="text-center text-wrap p-4">
                         <i class="fa-solid fa-chart-bar fa-3x mb-3"></i><!--icone-->
                         <h3>Analyse de Données</h3>
                         <p>Utilisation de données pour optimiser les performances en ligne et prendre des décisions
                             éclairées.</p>
-                    </div><!-- eof card-body -->
-                </div><!-- eof card -->
+                    </div>
 
-            </div><!-- eof card-deck -->
+                    <div class="text-center text-wrap p-4">
+                        <i class="fa-solid fa-code fa-3x mb-3"></i><!--icone-->
+                        <h3>UI & UX Design</h3>
+                        <p>Création d'interfaces utilisateur attrayantes et réactives avec les dernières
+                            technologies frontend.</p>
 
-            <div class="card-deck mt-4" data-aos="fade-right" data-aos-delay="1000" data-aos-duration="1000"><!-- 3ème ligne -->
-            <div class="card text-center border-0">
-                <div class="card-body">
-                    <i class="fa-solid fa-code fa-3x mb-3"></i><!--icone-->
-                    <h3>UI & UX Design</h3>
-                    <p>Création d'interfaces utilisateur attrayantes et réactives avec les dernières technologies frontend.</p>
-                </div><!-- eof card-body -->
-            </div><!-- eof card -->
-    
-            <div class="card text-center border-0">
-                <div class="card-body">
-                    <i class="fa-solid fa-database fa-3x mb-3"></i><!--icone-->
-                    <h3>Gestion de Bases de Données</h3>
-                    <p>Conception et gestion efficace de bases de données pour assurer la disponibilité et la sécurité des données.</p>
-                </div><!-- eof card-body -->
-            </div><!-- eof card -->
-    
-            <div class="card text-center border-0">
-                <div class="card-body">
-                    <i class="fa-solid fa-users fa-3x mb-3"></i><!--icone-->
-                    <h3>Marketing Digital</h3>
-                    <p>Stratégies de marketing numérique pour maximiser la visibilité en ligne et attirer un public ciblé.</p>
-                </div><!-- eof card-body -->
-            </div><!-- eof card -->
-            </div><!-- eof card deck -->
+                    </div>
 
-        </div><!-- eof parallax -->
+                    <div class="text-center text-wrap p-4">
+                        <i class="fa-solid fa-database fa-3x mb-3"></i><!--icone-->
+                        <h3>Gestion de Bases de Données</h3>
+                        <p>Conception et gestion efficace de bases de données pour assurer la disponibilité et la
+                            sécurité des données.</p>
+                    </div>
 
-    </div><!-- eof container -->
-</div><!-- eof bg-white -->
+                    <div class="text-center text-wrap p-4">
+                        <i class="fa-solid fa-users fa-3x mb-3"></i><!--icone-->
+                        <h3>Marketing Digital</h3>
+                        <p>Stratégies de marketing numérique pour maximiser la visibilité en ligne et attirer un
+                            public ciblé.</p>
+                    </div>
+
+
+                    <div class="text-center text-wrap p-4">
+                        <i class="fa-solid fa-desktop fa-3x mb-3"></i><!--icone-->
+                        <h3>Conception de Sites Web</h3>
+                        <p>Nous créons des sites web modernes et intuitifs pour représenter votre entreprise de
+                            manière
+                            professionnelle.</p>
+                    </div>
+
+                    <div class="text-center text-wrap p-4">
+                        <i class="fa-solid fa-paint-brush fa-3x mb-3"></i><!--icone-->
+                        <h3>Conception Graphique</h3>
+                        <p>Notre équipe de graphistes talentueux donne vie à votre marque avec des visuels
+                            accrocheurs
+                            et un design unique.</p>
+                    </div>
+
+                    <div class="text-center text-wrap p-4">
+                        <i class="fa-solid fa-cogs fa-3x mb-3"></i><!--icone-->
+                        <h3>Développement Web</h3>
+                        <p>Des solutions web personnalisées et évolutives pour répondre aux besoins spécifiques de
+                            votre
+                            entreprise.</p>
+                    </div>
+
+
+                </div><!-- eof container-expertise -->
+            </div><!-- eof main-expertise -->
+        </div><!-- eof container-fluid -->
+
+
+    </div><!-- eof bg-white -->
 
     <?php include '../inc.footer.php';?>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
-  AOS.init();
-</script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
