3b1e8d1223bd579eca57ed2e720db6836b7e9b6a
Amanda Héry-Prieto <amandaheryprieto@gmail.com>
2024-03-22 16:14:11 +0200

    Ajout filtre recherches annonces

--- inc.functions.php
+++ inc.functions.php
@@ -2,7 +2,7 @@
         /* incorporation de tous les avis depuis la BDD 
         **************************************************/
         // requête avec tri par date descendante
-    function getReviews() {
+function getReviews() {
     global $bdd; 
     
     $requete = $bdd->query('SELECT texte, auteur, note FROM avis');
@@ -48,33 +48,9 @@
 
         $requete->closeCursor();  // clôture de la requête
 
-                    }
+}
      
 
-        global $bdd;
-        /* incorporation de toutes les annonces depuis la BDD 
-          **************************************************/
-    
-          $requete = $bdd->query('SELECT categorie, titre, texte FROM annonces ORDER BY categorie ASC');
-          
-          // boucle while pour afficher toutes les entrées
-          while ($annonces = $requete->fetch()){
-              if (!$annonces) // On teste si la réponse à la requête est vide.
-              {
-                  echo 'La BDD n\'existe pas ou est vide.';
-                  break;
-              }
-              else
-              {   // affichage de toutes les entrées de la BDD
-                  echo '
-                        <div class="ad-card col-md-6 d-flex flex-column border shadow rounded m-auto py-2 ">
-                            <span class="bg-info text-light text-center border rounded-pill w-25 mx-2 ">'.$annonces['categorie'].'</span>
-                            <h2 class="text-center text-secondary ">'.$annonces['titre'].'</h2>
-                                <p class="text-justify p-3">'.$annonces['texte'].'</p></div>';
-              }
-          }
-          // clôture de la requête
-          $requete->closeCursor();
 
     // Fonction pour afficher les résultats d'une recherche
 	function getAds(){
